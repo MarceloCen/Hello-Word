3774b236424f28f48320dbd16b47ef0e6e1977f8
Marcelo Centurion <marcelo.centurion.med@gmail.com>
2021-12-01 11:25:14 -0300

    aprendiendo sobre git

---
+++ main.js
@@ -0,0 +1,10 @@
+document.querySelector('.menubtn').addEventListener('click', () => {
+    document.querySelector('.navmenu').classList.toggle('show');
+});
+
+ScrollReveal().reveal('.showcase', { delay: 500});
+ScrollReveal().reveal('.newcards', { delay: 500});
+ScrollReveal().reveal('.bannerUne', { delay: 500});
+ScrollReveal().reveal('.bannerTwo', { delay: 500});
+ScrollReveal().reveal('.social', { delay: 500});
+ScrollReveal().reveal('.footer-conteiner', { delay: 500});
